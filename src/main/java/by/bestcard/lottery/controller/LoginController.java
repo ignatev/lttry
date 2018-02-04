451f0d89e7743247f076de86c17faeb7bc9ad8a5
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-02-04 23:59:27 +0300

    add sms verification method

--- src/main/java/by/bestcard/lottery/controller/LoginController.java
+++ src/main/java/by/bestcard/lottery/controller/LoginController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class LoginController {
 
@@ -23,7 +25,7 @@ public class LoginController {
     }
 
     @PostMapping(value = "/login")
-    public String submitMobileNumber(@ModelAttribute MobileNumber mobileNumber) {
+    public String submitMobileNumber(@ModelAttribute MobileNumber mobileNumber, HttpServletRequest request) {
         RestTemplate restTemplate = new RestTemplate();
         String number = mobileNumber.getMobileNumber();
         String url = "https://phoneverification.devinotele.com/GenerateCode";
@@ -35,6 +37,7 @@ public class LoginController {
         HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
         String answer = restTemplate.postForObject(url, entity, String.class);
         log.info(answer);
+        request.getSession().setAttribute("mobileNumber", mobileNumber);
         return "redirect:/sms-code-verification";
     }
 
@@ -42,5 +45,23 @@ public class LoginController {
     public String smsVerificationForm() {
         return "sms-code-verification";
     }
+
+    @PostMapping("/sms-code-verification")
+    public String smsCodeValidation(@ModelAttribute SmsCode smsCode, HttpServletRequest request) {
+        MobileNumber mobileNumber = (MobileNumber) request.getSession().getAttribute("mobileNumber");
+        String requestJson = "{\"DestinationNumber\":" + mobileNumber.getMobileNumber() + "\", \"Code\":" + smsCode.getSmsCode() + "\"}";
+        log.info(requestJson);
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://phoneverification.devinotele.com/CheckCode";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-ApiKey", "SypxhGPcE37R12XTX121id8jUvn70L4S");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
+        String answer = restTemplate.postForObject(url, entity, String.class);
+        log.info(answer);
+        return "redirect:/";
+    }
+
 }
 
---
+++ src/main/java/by/bestcard/lottery/controller/SmsCode.java
@@ -0,0 +1,13 @@
+package by.bestcard.lottery.controller;
+
+public class SmsCode {
+    private String smsCode;
+
+    public String getSmsCode() {
+        return smsCode;
+    }
+
+    public void setSmsCode(String smsCode) {
+        this.smsCode = smsCode;
+    }
+}
